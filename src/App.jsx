d728d2de6200eb71704c99c737e48202e32d527c
Caique Rodrigues <142418237+Caique0Og@users.noreply.github.com>
2025-03-13 21:33:16 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import LoginPage from './views/LoginPage'
+import { AuthProvider } from './contexts/AuthContext'
+import { useAuth } from './contexts/AuthContext'
+import MainPage from './views/MainPage'
+
+function App() {
+ 
+
+  return (
+    <>
+    <AuthProvider>
+      <AuthContext/>
+    </AuthProvider>
+    
+
+    </>
+  )
+}
+
+function AuthContext(){
+  const{ user } = useAuth()
+
+  return(
+    <>
+    { user ? <MainPage/> : <LoginPage/>}
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+
+  
+        apiKey: "AIzaSyA5U16ue8v7IJuVi6m9zUZI9PAGxWG3kzE",
+        authDomain: "etec2025-58fa6.firebaseapp.com",
+        projectId: "etec2025-58fa6",
+        storageBucket: "etec2025-58fa6.firebasestorage.app",
+        messagingSenderId: "42889120956",
+        appId: "1:42889120956:web:b7d8b384e122578a0f54b9",
+        measurementId: "G-J582V96BN8"
+ 
+  
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+
+export const db = getFirestore(app);
